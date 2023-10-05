58c6e96b31986a5e364713bd4b78d4aeb3739916
Αργυροκαστρίτης Ιωάννης - Σπυρίδων <94742350+Argyrokastritis@users.noreply.github.com>
2023-10-05 21:25:26 +0300

    H-M radiofrequencies python project

--- coordinates_input_GUI.py
+++ coordinates_input_GUI.py
@@ -1,29 +1,3 @@
-# import PySimpleGUI as sg
-#
-# class coordinates_input_GUI:
-#     def __init__(self):
-#         sg.theme('DarkAmber')
-#
-#         layout = [[sg.Text('Enter the latitude and longitude values below:')],
-#                   [sg.Text('Latitude:'), sg.InputText(size=(20, 1)), sg.Text('Longitude:'), sg.InputText(size=(20, 1))],
-#                   [sg.Button('Submit'), sg.Button('Cancel')]]
-#
-#         self.window = sg.Window('Location Input', layout)
-#
-#     def get_location(self):
-#         while True:
-#             event, values = self.window.read()
-#             if event == sg.WIN_CLOSED or event == 'Cancel':
-#                 break
-#             elif event == 'Submit':
-#                 latitude = float(values[0])
-#                 longitude = float(values[1])
-#                 location = [latitude, longitude]
-#                 print("The location you entered is:", location)
-#                 return location
-#
-#         self.window.close()
-#
 import tkinter as tk
 
 class coordinates_input_GUI:
