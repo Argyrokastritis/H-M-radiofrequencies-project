933202c90a771b970225446403f27ea23ad19aef
Αργυροκαστρίτης Ιωάννης - Σπυρίδων <94742350+Argyrokastritis@users.noreply.github.com>
2023-10-05 22:12:59 +0300

    H-M radiofrequencies python project

--- coordinates_input_GUI.py
+++ coordinates_input_GUI.py
@@ -1,23 +1,29 @@
 import tkinter as tk
+from tkinter import Tk, PhotoImage
 
 class coordinates_input_GUI:
     def __init__(self):
         self.window = tk.Tk()
+        icon = PhotoImage(file='earth.png')
+        self.window.iconphoto(True, icon)
         self.window.title("Location Input")
-        self.window.geometry("400x100")
+        self.window.geometry("270x110")
+        self.window.configure(bg="light blue")
 
-        tk.Label(self.window, text="Enter the latitude and longitude values below:").grid(row=0, column=0, columnspan=2)
+        tk.Label(self.window, text="Enter the latitude and longitude values below:",bg="light blue").grid(row=0, column=0, columnspan=2)
 
-        tk.Label(self.window, text="Latitude:").grid(row=1, column=0)
-        self.latitude_entry = tk.Entry(self.window)
+        tk.Label(self.window, text="Latitude:",bg="light blue").grid(row=1, column=0)
+        self.latitude_entry = tk.Entry(self.window, fg='blue')
         self.latitude_entry.grid(row=1, column=1)
 
-        tk.Label(self.window, text="Longitude:").grid(row=2, column=0)
-        self.longitude_entry = tk.Entry(self.window)
+        tk.Label(self.window, text="Longitude:",bg="light blue").grid(row=2, column=0)
+        self.longitude_entry = tk.Entry(self.window, fg='blue')
         self.longitude_entry.grid(row=2, column=1)
 
-        tk.Button(self.window, text="Submit", command=self.submit).grid(row=3, column=0)
-        tk.Button(self.window, text="Cancel", command=self.cancel).grid(row=3, column=1)
+        tk.Button(self.window, text="Submit", command=self.submit, bg="light yellow", activebackground="yellow").grid(
+            row=3, column=0, pady=10)
+        tk.Button(self.window, text="Cancel", command=self.cancel, bg="light salmon", activebackground="salmon").grid(
+            row=3, column=1, pady=10)
 
     def submit(self):
         latitude_str = self.latitude_entry.get()
